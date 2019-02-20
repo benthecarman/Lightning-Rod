f148c363ba760a617e25d93459a4174d27efb24d
Ben Carman <benthecarman@live.com>
2019-02-20 02:06:52 -0600

    Formatting

--- src/rpcconnection.h
+++ src/rpcconnection.h
@@ -1,19 +1,19 @@
 #ifndef RPCConnection_H
 #define RPCConnection_H
 
-class RPCConnection {
-    
+class RPCConnection
+{
     std::string url, userpwd;
 
-    public:
-        RPCConnection(std::string, std::string);
-        void setURL(std::string);
-        std::string getURL();
-        void setUserPwd(std::string);
-        std::string getUserPwd();
-        std::string execute(std::string, std::string);
-        std::string execute(std::string);
-        std::string execute();
+  public:
+    RPCConnection(std::string, std::string);
+    void setURL(std::string);
+    std::string getURL();
+    void setUserPwd(std::string);
+    std::string getUserPwd();
+    std::string execute(std::string, std::string);
+    std::string execute(std::string);
+    std::string execute();
 };
 
 #endif
