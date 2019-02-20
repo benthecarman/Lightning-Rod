13e9747030977ee7cfbbb725e6e67d383c46a75b
Ben Carman <benthecarman@live.com>
2019-02-20 15:32:20 -0600

    fix

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ CC=g++
 CFLAGS=-w -pthread
 ENDFLAGS=-lcurl
 
-default: rod test
+default: rod
 
 rod: src/rod.cpp
 	$(CC) $(CFLAGS) src/rod.cpp src/config.cpp src/server.cpp src/rpcconnection.cpp -o rod.o $(ENDFLAGS)
--- src/config.cpp
+++ src/config.cpp
@@ -1,35 +1,4 @@
 #include <string>
-#include <cstring>
-
-#include "src/rpcconnection.h"
-
-int main(int argc, char *argv[])
-{
-    std::string port = "8331";
-    if (argc > 1)
-    {
-        int i;
-        for (i = 1; i < argc; ++i)
-        {
-            std::string tmp(argv[i], 0, strlen(argv[i]));
-
-            if (tmp.find("--port=") == 0)
-            {
-                int pos = tmp.find("--port=");
-                port = tmp.substr(pos + 7);
-            }
-        }
-    }
-
-    RPCConnection rpc("http://127.0.0.1:" + port + "/", "benLinuxPC828:fgCy5QaEgu&C&q$7");
-
-    std::string rev = rpc.execute();
-
-    printf("%s\n", rev.c_str());
-
-    return 0;
-}
-#include <string>
 
 #include "config.h"
 
