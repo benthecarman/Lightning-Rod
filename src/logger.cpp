7d9c63fee69dbb1bdb4417713a6afbe2e8e88a6b
Ben Carman <benthecarman@live.com>
2019-06-08 14:46:46 -0500

    logger stdout aligns new lines correctly

--- src/logger.cpp
+++ src/logger.cpp
@@ -25,44 +25,77 @@ void initLogger()
         boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0));
 }
 
+void findAndReplaceAll(std::string &data, std::string toSearch, std::string replaceStr)
+{
+    // Get the first occurrence
+    size_t pos = data.find(toSearch);
+
+    // Repeat till end is reached
+    while (pos != std::string::npos)
+    {
+        // Replace this occurrence of Sub String
+        data.replace(pos, toSearch.size(), replaceStr);
+        // Get the next occurrence from the current position
+        pos = data.find(toSearch, pos + replaceStr.size());
+    }
+}
+
 void logTrace(std::string lg)
 {
-    if (config.getDebugLevel() <= DebugLevel::trace && !config.isDaemon())
-        std::cout << "[Trace]\t\t" << lg << std::endl;
     BOOST_LOG_TRIVIAL(trace) << lg;
+    if (config.getDebugLevel() <= DebugLevel::trace && !config.isDaemon())
+    {
+        findAndReplaceAll(lg, "\n", "\n\t\t");
+        std::cout << "[Trace]\t\t" << lg << std::endl;
+    }
 }
 
 void logDebug(std::string lg)
 {
-    if (config.getDebugLevel() <= DebugLevel::debug && !config.isDaemon())
-        std::cout << "[Debug]\t\t" << lg << std::endl;
     BOOST_LOG_TRIVIAL(debug) << lg;
+    if (config.getDebugLevel() <= DebugLevel::debug && !config.isDaemon())
+    {
+        findAndReplaceAll(lg, "\n", "\n\t\t");
+        std::cout << "[Debug]\t\t" << lg << std::endl;
+    }
 }
 
 void logInfo(std::string lg)
 {
-    if (config.getDebugLevel() <= DebugLevel::info && !config.isDaemon())
-        std::cout << "[Info]\t\t" << lg << std::endl;
     BOOST_LOG_TRIVIAL(info) << lg;
+    if (config.getDebugLevel() <= DebugLevel::info && !config.isDaemon())
+    {
+        findAndReplaceAll(lg, "\n", "\n\t\t");
+        std::cout << "[Info]\t\t" << lg << std::endl;
+    }
 }
 
 void logWarning(std::string lg)
 {
-    if (config.getDebugLevel() <= DebugLevel::warning && !config.isDaemon())
-        std::cout << "[Warning]\t" << lg << std::endl;
     BOOST_LOG_TRIVIAL(warning) << lg;
+    if (config.getDebugLevel() <= DebugLevel::warning && !config.isDaemon())
+    {
+        findAndReplaceAll(lg, "\n", "\n\t");
+        std::cout << "[Warning]\t" << lg << std::endl;
+    }
 }
 
 void logError(std::string lg)
 {
-    if (config.getDebugLevel() <= DebugLevel::error && !config.isDaemon())
-        std::cerr << "[Error]\t\t" << lg << std::endl;
     BOOST_LOG_TRIVIAL(error) << lg;
+    if (config.getDebugLevel() <= DebugLevel::error && !config.isDaemon())
+    {
+        findAndReplaceAll(lg, "\n", "\n\t\t");
+        std::cerr << "[Error]\t\t" << lg << std::endl;
+    }
 }
 
 void logFatal(std::string lg)
 {
-    if (config.getDebugLevel() <= DebugLevel::fatal && !config.isDaemon())
-        std::cerr << "[Fatal]\t\t" << lg << std::endl;
     BOOST_LOG_TRIVIAL(fatal) << lg;
+    if (config.getDebugLevel() <= DebugLevel::fatal && !config.isDaemon())
+    {
+        findAndReplaceAll(lg, "\n", "\n\t\t");
+        std::cerr << "[Fatal]\t\t" << lg << std::endl;
+    }
 }
